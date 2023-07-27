dc803a5600d8803659d76584839ef83d0cb028cf
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2023-07-27 12:03:11 -0700

    automate PWA config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "Business Card Sebastiaan Jansen",
+        short_name: "Business Card",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        start_url: "/index.html",
+        orientation: "portrait",
+        display: "fullscreen",
+      },
+    }),
+  ],
+});
